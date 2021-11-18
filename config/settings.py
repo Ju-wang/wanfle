9d61c641ad073b2b21618f1ead5fdb1043d09b14
Ju-wang <charles09099@gmail.com>
2021-11-18 21:40:42 +0900

    test

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-zbbni%#-c#(k=q5o@*jprdaud+tb%&sfe(sm(p%(fibx&-e=(^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["wanfle.eba-mxajfdwz.ap-northeast-2.elasticbeanstalk.com"]
+ALLOWED_HOSTS = ["*"]
 
 AUTH_USER_MODEL = "users.User"
 
@@ -135,7 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 MEDIA_URL = "/media/"
